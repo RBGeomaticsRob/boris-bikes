7724da069fde85aace49981a6aa1a41239712a3c
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-03-18 16:12:24 +0000

    Extended the feature test and unit test with added functionality of docking at docking_stations

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_accessor :bike
+  alias_method :dock, :bike=
+
   def release_bike
-    Bike.new
+    fail 'No Bikes Available' unless @bike
+    @bike
   end
+  # def dock(bike)
+  #   @bike = bike
+  # end
 end
