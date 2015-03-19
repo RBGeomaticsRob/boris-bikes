e02053ba5430bbda664d7d73c7a8468f4f7dd524
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-03-19 09:31:12 +0000

    end of stage 4

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,13 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_writer :bike
+  alias_method :dock, :bike=
   def release_bike
-    Bike.new
+    fail 'No Bikes Available' unless @bike
+    @bike
   end
+  # def dock(bike)
+  #   @bike = bike
+  # end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,15 @@
 require 'docking_station'
+
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
   it 'releases bikes that are not broken' do
+    subject.dock double(:bike, broken?: false)
     bike = subject.release_bike
     expect(bike).not_to be_broken
+  end
+
+  it 'can dock a bike' do
+    expect(subject.dock(:bike)).to eq :bike
   end
 end
